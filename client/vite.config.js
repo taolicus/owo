2ee79ea143f00dfc91aa81292af636f36843db1b
Tao <taozamorano@gmail.com>
2024-09-17 12:29:46 +0100

    add base url to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '../', 'VITE_');
 
   return {
+    base: env.VITE_BASE_URL || '/',
     build: {
       outDir: env.VITE_OUTPUT_DIR || 'dist'
     }
